e50a73d5fd598b82299625634cc95628e17990c8
DatGuyShorty <darkkrix@gmail.com>
2025-06-03 21:51:12 +0200

    Enhance volcano map application: add image retrieval, improve marker categorization, and update HTML structure

--- visionapp.py
+++ visionapp.py
@@ -1,50 +1,103 @@
-
+from bs4 import BeautifulSoup
+import requests
 import pandas
-import geopy
 import folium
 from folium import plugins
 
-
-
 html = """
 <b>Volcano name:</b><br>
-<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
-<img src="https://www.google.com/search?tbm=isch&q=%s">
+<a href="https://www.google.com/search?q=%s" target="_blank">%s</a><br> 
+<a href="https://en.wikipedia.org/wiki/%s" target="_blank">%s</a><br>
+%s
 <b>Height: </b> %s m
 <b>Type:</b> %s
+<b>Status:</b> %s
 """
-
+# https://www.google.com/search?q=%Valles%20Caldera%
 #color deciding function
 def color_producer(elevation):
     if elevation < 1000:
         return "green"
     elif 1000 <= elevation < 3000:
         return "orange"
-    elif 3000 <= elevation < 5000:
+    elif 3000 < elevation :
         return "red"
 
+def get_volcano_image_html(volcano_name):
+    wiki_url = f"https://en.wikipedia.org/wiki/{volcano_name.replace(' ', '_')}"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    
+    res = requests.get(wiki_url, headers=headers)
+    soup =  BeautifulSoup(res.text, "html.parser")
+    print("Wiki link: ", wiki_url)
+        # Look for the infobox table
+    infobox = soup.find("table", class_="infobox")
+    if infobox is None:
+        # No infobox → no image available
+        return f"NO INFOBOX FOUND"
+    
+    img = soup.find("table", class_="infobox").find("img")
+    if img:
+        img_url = "https:" + img["src"]
+        html = f'''
+        <img src="{img_url}" alt="{volcano_name}" style="max-width:400px;"><br>
+        '''
+        return html
+    else:
+        return f'NO IMAGE FOUND'
+    
 
+    
 map = folium.Map(location=[47.116386, -101.299591], zoom_start=4)
-fg = folium.FeatureGroup(name="Volcanoes") 
+fg1 = folium.FeatureGroup(name="Low Volcanoes")
+fg2 = folium.FeatureGroup(name="Medium Volcanoes")
+fg3 = folium.FeatureGroup(name="High Volcanoes")
 #variables
-data = pandas.read_csv("Volcanoes.txt")
+data = pandas.read_csv(r"C:\Users\darkk\Desktop\VolcanoMap-main\Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 name = list(data["NAME"])
 elev = list(data["ELEV"])
 vtype = list(data["TYPE"])
+status = list(data["STATUS"])
+
+total_entries = len(lat)
+print("Num. volcanoes to process: ", total_entries)
 
 #for each entry in zip() add marker to the fg group
-for lt, ln, nm, el, tp in zip(lat, lon, name, elev, vtype):
-    iframe = folium.IFrame(html=html % (nm, nm,nm, el, tp), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(
-        iframe), icon=folium.Icon(color=color_producer(el),)))
+for i, (lt, ln, nm, el, tp, st) in enumerate(zip(lat, lon, name, elev, vtype, status), start=1):
+    print("\033c")
+    # Print volcano name
+    print("Volcano name: ", nm)
+    # Print img link or error
+    img_src = get_volcano_image_html(nm)
+    print("Img link: ", img_src)
+    #Print progress
+    progress = (i / total_entries) * 100
+    print(f"Progress: {progress:.2f}%")
 
+    #create iframe with html
+    iframe = folium.IFrame(html=html % (nm, nm, nm, nm, img_src, el, tp, st), width=400, height=600)
+
+    #create marker with location, popup and icon
+    if el < 1000:    #Green markers
+        fg1.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(
+            iframe), icon=folium.Icon(color=color_producer(el),)))
+    elif 1000 <= el < 3000:  #Orange markers
+        fg2.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(
+            iframe), icon=folium.Icon(color=color_producer(el),))) 
+    elif 3000 <= el:  #Red markers
+        fg3.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(
+            iframe), icon=folium.Icon(color=color_producer(el),))) 
+        
 #add_child to map
-map.add_child(fg)
+map.add_child(fg1) #add low volcanoes feature group
+map.add_child(fg2) #add medium volcanoes feature group
+map.add_child(fg3) #add high volcanoes feature group  
 #minimap inicialization 
 minimap = plugins.MiniMap()
 map.add_child(minimap)
 map.add_child(folium.LayerControl()) 
 #save the map
 map.save("map1.html")
+input() 
